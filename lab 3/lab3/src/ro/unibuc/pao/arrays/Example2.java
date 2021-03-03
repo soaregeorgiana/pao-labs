cdf6e215ce101432e091b1e1182c95018074d79e
georgianafrusescu <georgiana.frusescu@endava.com>
2021-03-03 12:51:12 +0200

    Remove some of the automatically replacement made by the IDE when changed the package, Using the example suggested by the students for the comparison exercise

--- lab 3/lab3/src/ro/unibuc/pao/arrays/Example2.java
+++ lab 3/lab3/src/ro/unibuc/pao/arrays/Example2.java
@@ -3,9 +3,9 @@ package ro.unibuc.pao.arrays;
 import java.util.Arrays;
 
 /**
- * Write a program that compares two ro.unibuc.pao.arrays:
+ * Write a program that compares two arrays:
  * <p>
- * Arrays.compare(int[]a, int[]b) -> Compares two ro.unibuc.pao.arrays lexicographically.
+ * Arrays.compare(int[]a, int[]b) -> Compares two  arrays lexicographically.
  * Returns:
  * - the value 0 if the first and second array are equal and contain the same elements in the same order;
  * - a value less than 0 if the first array is lexicographically less than the second array;
@@ -13,29 +13,29 @@ import java.util.Arrays;
  * This method can be used by all primitive types.
  * <p>
  * Arrays.equals(int[]a, int[]b)
- * Returns true if two specified ro.unibuc.pao.arrays are equal to one another
- * Two ro.unibuc.pao.arrays are considered equal if both ro.unibuc.pao.arrays contain the same number of elements,
- * and all corresponding pairs of elements in the two ro.unibuc.pao.arrays are equal
+ * Returns true if two specified arrays are equal to one another
+ * Two  arrays are considered equal if both arrays contain the same number of elements,
+ * and all corresponding pairs of elements in the two arrays are equal
  * This method can be used by all primitive types.
  */
 public class Example2 {
 
     public static void main(String[] args) {
-        // define the two ro.unibuc.pao.arrays
+        // define the two arrays
 
-        int[] first = {1, 2, 3, 4};
-        int[] second = {1, 2, 3, 6};
+        int[] first = {2, 3, 4};
+        int[] second = {1, 1, 1, 1, 1, 1};
 
         int result = Arrays.compare(first, second);
 
-        if(result == 0 ){
-            System.out.println("The ro.unibuc.pao.arrays are equal");
+        if (result == 0) {//result = 1
+            System.out.println("The arrays are equal");
         } else {
-            System.out.println("The ro.unibuc.pao.arrays are not equal");
+            System.out.println("The arrays are not equal");
         }
 
         boolean isEqual = Arrays.equals(first, second);
-        System.out.println("Are these ro.unibuc.pao.arrays equal? " + isEqual);
+        System.out.println("Are these arrays equal? " + isEqual);
     }
 
 }
